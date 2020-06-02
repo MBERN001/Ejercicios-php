69f46fdab66cd359b82cbadce629e83c5855b5cc
Milton Bernardez <castillojavier2017@gmail.com>
2020-06-02 12:10:23 -0600

    primer cambio

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+   <h1>Ejercicios de PHP</h1>
+
+   <ul>
+   <li><a href="Ejercicio2.php" target = "_new">Ejercicio 2</a></li>
+   <li><a href="Ejercicio3.php" target = "_new">Ejercicio 3</a></li>
+   <li><a href="Ejercicio4.php" target = "_new">Ejercicio 4</a></li>
+   <li><a href="Ejercicio5.php" target = "_new">Ejercicio 5</a></li>
+   <li><a href="Ejercicio6.php" target = "_new">Ejercicio 6</a></li>
+   <li><a href="Ejercicio7.php" target = "_new">Ejercicio 7</a></li>
+   </ul>
+</body>
+</html>
