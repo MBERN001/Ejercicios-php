89559bedc4c4029badc4237a27c26cca5442fe12
Milton Bernardez <castillojavier2017@gmail.com>
2020-06-02 14:45:47 -0600

    Update Ejercicio6.php
    cambio 4

--- Ejercicio6.php
+++ Ejercicio6.php
@@ -7,11 +7,11 @@
 
 header ("Content-type: text/html;charset=\"utf-8\"");
   
-echo "<h1>.$_GET["clase"].</h1>";
-echo "<h2>.$_GET["periodo"].</h2>";
+echo "<h1>".$_GET["clase"]."</h1>";
+echo "<h2>".$_GET["periodo"]."</h2>";
 
-echo "<h1>.$_GET["nombre"]</h1>";
-echo "<h2>.$_GET["apellido"]</h2>";
+echo "<h1>".$_GET["nombre"]."</h1>";
+echo "<h2>".$_GET["apellido"]."</h2>";
 
 if (is_numeric($_GET)["Edad"] && $_GET["edad"]>=1) {
 echo "<h3>Bienvenido" $_GET["nombre"]." ".$_GET["apellido"]."</h3>";
